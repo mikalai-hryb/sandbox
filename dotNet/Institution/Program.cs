408f2f7b41821f814dbd9c47823263f37386002a
Mikalai Hryb <mikalai.hryb@gmail.com>
2020-03-24 00:15:05 +0300

    Add solution Intitution and AHyman class

---
+++ dotNet/Institution/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Institution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentA studentA = new StudentA("female", "6", "student", "Аня", "1A" );
+            TeacherA teacherA = new TeacherA("female", "25", "teacher", "Светлана Ивановна", "1A" );
+            var answer1 = studentA.GetAwayFromTheAnswer(teacherA);
+            var answer2 = teacherA.GetAwayFromTheAnswer(studentA);
+            Console.WriteLine(answer1);
+            Console.WriteLine(answer2);
+        }
+    }
+}
