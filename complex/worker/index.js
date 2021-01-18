7a72b4ce81558860879a85abc64f40b58edb2dfc
Mikalai Hryb <mikalai.hryb@gmail.com>
2021-01-18 22:09:59 +0300

    add complex

---
+++ complex/worker/index.js
@@ -0,0 +1,20 @@
+const keys = require('./keys');
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+const redisPublisher = redisClient.duplicate();
+
+function fib(index) {
+    if (index < 2) return 1;
+    return fib(index - 1) + fib(index - 2);
+}
+
+redisPublisher.on('message', (channel, message) => {
+   redisClient.hset('values', message, fib(parseInt(message )));
+});
+
+redisPublisher.subscribe('insert');
