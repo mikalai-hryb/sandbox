38d63a1aa35f490211b3b21310cc9621c600c861
Mikalai Hryb <mikalai.hryb@gmail.com>
2021-01-18 19:43:48 +0300

    chore in App.js

--- docker/frontend/src/App.js
+++ docker/frontend/src/App.js
@@ -8,7 +8,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          But it's up to you or not. Rdf
         </p>
         <a
           className="App-link"
