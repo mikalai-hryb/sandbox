5f5f6f159699ae79ec183e2a9203804ad94ba6f2
Mikalai Hryb <mikalai.hryb@gmail.com>
2021-01-19 18:33:55 +0300

    complex:
    * add docker-compose.yml
    * add nginx default config
    * make cleanup in Dockerfile.dev files

--- docker/complex/server/keys.js
+++ docker/complex/server/keys.js
@@ -3,7 +3,7 @@ module.exports = {
     redisPort: process.env.REDIS_PORT,
     pgUser: process.env.PGUSER,
     pgPassword: process.env.PGPASSWORD,
-    pgDatabase: process.env.PGUSER,
+    pgDatabase: process.env.PGDATABASE,
     pgHost: process.env.PGHOST,
     pgPort: process.env.PGPORT
 };
--- docker/complex/worker/index.js
+++ docker/complex/worker/index.js
@@ -2,19 +2,19 @@ const keys = require('./keys');
 const redis = require('redis');
 
 const redisClient = redis.createClient({
-    host: keys.redisHost,
-    port: keys.redisPort,
-    retry_strategy: () => 1000
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
 });
 const redisPublisher = redisClient.duplicate();
 
 function fib(index) {
-    if (index < 2) return 1;
-    return fib(index - 1) + fib(index - 2);
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
 }
 
 redisPublisher.on('message', (channel, message) => {
-   redisClient.hset('values', message, fib(parseInt(message )));
+  redisClient.hset('values', message, fib(parseInt(message )));
 });
 
 redisPublisher.subscribe('insert');
